525532a50b9701573a60ed70275a866d6d58e464
Nate Case <nacase@gmail.com>
2014-09-17 16:11:04 -0500

    cmpe.py: Update comments in header
    Now that this script supports its own shell, there's no real reason
    to include this module in every Python invocation, so get rid of
    those instructions.
    
    Signed-off-by: Nate Case <nacase@gmail.com>

--- cmpe.py
+++ cmpe.py
@@ -3,17 +3,11 @@
 # Python convenience functions for computer engineering
 #
 # These functions are intended to be used in an interactive Python
-# shell (i.e., like a calculator).  To automatically include these
-# functions for all Python shells, add the following to ~/.python.start.py:
+# shell (i.e., like a calculator).
 #
-#     import os, sys
-#     # Append home directory to PYTHONPATH
-#     sys.path.append(os.path.expanduser("~"))
-#     from cmpe import *
-#
-# Alternatively, you can also simply invoke this script and it will
-# launch a basic Python interactive shell with these functions available
-# in the global namespace.
+# You can import this module in an interactive shell, or alternatively,
+# you can simply invoke this script and it will launch a basic Python
+# interactive shell with these functions available in the global namespace.
 #
 # To update to the latest version of this script, simply run:
 #
